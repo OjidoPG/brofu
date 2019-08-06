fb506ab9ae5c4761b3fcb1288163266a52ae7ce1
Pascal <giraud.pascal@outlook.com>
2019-08-06 21:15:23 +0200

    mise en place header

--- src/layouts/BrocanteHeaderLayout.vue
+++ src/layouts/BrocanteHeaderLayout.vue
@@ -1,43 +1,66 @@
 <template>
-    <v-card class="overflow-hidden">
-        <v-app-bar
-                absolute
-                color="indigo darken-2"
-                dark
-                shrink-on-scroll
-                prominent
-                scroll-target="#scrolling-techniques"
-        >
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app id="inspire">
+        <div>
+            <v-app-bar
+                    color="light-blue darken-3"
+                    dark
+            >
+                <div class="col-md-10 offset-md-1 text-center font-weight-bold">
+                    <v-toolbar-title class="title">Brocante</v-toolbar-title>
+                </div>
+                <div class="col-md-1 text-md-right">
+                    <v-menu>
+                        <template v-slot:activator="{ on }">
+                            <v-btn icon v-on="on">
+                                <v-icon>mdi-dots-vertical</v-icon>
+                            </v-btn>
+                        </template>
 
-            <v-toolbar-title>Title</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-            <v-btn icon>
-                <v-icon>search</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-                <v-icon>favorite</v-icon>
-            </v-btn>
-
-            <v-btn icon>
-                <v-icon>more_vert</v-icon>
-            </v-btn>
-        </v-app-bar>
-        <v-sheet
-                id="scrolling-techniques"
-                class="overflow-y-auto"
-                max-height="600"
-        >
-            <v-container style="height: 200px;"></v-container>
-        </v-sheet>
-    </v-card>
+                        <v-list>
+                            <v-list-item
+                                    v-for="option in options"
+                                    :key="option.message"
+                                    @click="aiguillage(option.message)"
+                            >
+                                <v-list-item-title>{{ option.message }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </div>
+            </v-app-bar>
+        </div>
+    </v-app>
 </template>
 
 <script>
     export default {
-        name: "BrocanteHeaderLayout"
+        name: "BrocanteHeaderLayout",
+        data (){
+            return{
+                options:[
+                    {message:'Nous rejoindre'},
+                    {message:'Administrateurs'}
+                ]
+            }
+    },
+        methods : {
+            aiguillage(data){
+                switch (data) {
+                    case 'Nous rejoindre' :
+                        alert('Nous rejoindre');
+                        break;
+                    case'Administrateurs':
+                        alert('Administrateurs');
+                        break;
+                    default:
+                        alert('Default');
+                        break;
+                }
+            }
+        }
     };
 </script>
+
+<style>
+
+</style>
