9b09212167c021882c9ebf3d1d65356d3e087d0a
OjidoPG <giraud.pascal@outlook.com>
2019-08-06 13:42:04 +0200

    mise en place page accueil avec vuetify

---
+++ src/layouts/BrocanteHeaderLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-card class="overflow-hidden">
+        <v-app-bar
+                absolute
+                color="indigo darken-2"
+                dark
+                shrink-on-scroll
+                prominent
+                scroll-target="#scrolling-techniques"
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>favorite</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>more_vert</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-sheet
+                id="scrolling-techniques"
+                class="overflow-y-auto"
+                max-height="600"
+        >
+            <v-container style="height: 200px;"></v-container>
+        </v-sheet>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "BrocanteHeaderLayout"
+    };
+</script>
---
+++ src/views/Accueil.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="Accueil">
+
+        <!-- Header Brocante -->
+        <BrocanteHeaderLayout></BrocanteHeaderLayout>
+
+        <!-- insertion de la card bootstrap -->
+        <h1>Corps de l'accueil</h1>
+
+        <!-- Footer Accueil -->
+        <BrocanteFooterLayout></BrocanteFooterLayout>
+
+    </div>
+</template>
+
+<script>
+    import BrocanteHeaderLayout from '@/layouts/BrocanteHeaderLayout.vue';
+    import BrocanteFooterLayout from '@/layouts/BrocanteFooterLayout.vue';
+    export default {
+        name: "Accueil",
+        components : {
+            BrocanteHeaderLayout,
+            BrocanteFooterLayout
+        }
+    };
+</script>
